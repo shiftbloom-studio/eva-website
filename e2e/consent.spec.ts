9846453d0cbd36bcce7f079919974499f77d33e6
Fabian 'Mekphen' Zimber <Fabian@Zentros-Gaming.de>
2026-01-15 05:10:36 +0100

    refactor: reorder import statements for consistency

--- e2e/consent.spec.ts
+++ e2e/consent.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test.describe('Consent & Settings', () => {
   // Override global storageState seeding so we can validate first-visit behavior.
